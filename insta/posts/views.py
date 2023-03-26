5a754385182c526cabb986264e50ebec1f244144
fishphobiagg <dlauddn6211@icloud.com>
2023-03-27 00:55:31 +0900

    insta clone

--- insta/posts/views.py
+++ insta/posts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def index(request):
-    return render(request, ('posts/index.html'))
+    post = Post.objects.all()
+
+    context = {
+        'post':post
+    }
+    return render(request, ('posts/index.html'), context)
